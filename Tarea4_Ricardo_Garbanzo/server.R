68f9974d611344c351d0c064eb1ecccf879458e8
Ricardo Garbanzo <rjgarbanzo@gmail.com>
2020-02-22 18:58:57 -0600

    No me da la gana hacer mas

--- Tarea4_Ricardo_Garbanzo/server.R
+++ Tarea4_Ricardo_Garbanzo/server.R
@@ -28,19 +28,25 @@ function(input, output, session) {
   "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
   
   par(mar = c(5.1, 4.1, 0, 1))
+  
   ggplot(data = selectedData(), mapping = aes_string(x = input$xcol, y = input$ycol, color = clusters()$cluster)) +
-    geom_point(mapping = aes_string(x = input$xcol,y = input$ycol),color = clusters()$clusters)
+    geom_point()+
+    labs(title = "Cantidad de clusters utilizados:",
+         subtitle = input$clusters)+
+    theme(
+      plot.title = element_text(color = "Blue", size = 32, face = "bold"),
+      plot.subtitle = element_text(color = "Red", size = 32)
+    )
+  
    })
 }
 
 
 
-#######################
+# ggplot(data = selectedData(), mapping = aes_string(x = input$xcol, y = input$ycol, color = clusters()$cluster)) +
+#   geom_point()
 
-# iris_clustered <- data.frame(selectedData, cluster=factor(clusters$cluster))
-# ggplot(iris_clustered, aes(x=Sepal.Length, y=Sepal.Width, color=cluster)) + 
-#   geom_point() 
-
+# ggtitle(input$clusters, "Clusters")
 
 #############################
 # output$plot1 <- renderPlot({
